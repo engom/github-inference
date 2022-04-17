e9fc73f3e37fd7ecc66314544f6f048b2040f5e8
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-04-18 00:25:22 +0200

    add dog image

--- tests/test_predict_model.py
+++ tests/test_predict_model.py
@@ -1,11 +1,11 @@
 # import predict_saved_model from src
 from src.predicter import predict_saved_model
-
+from pytest import approx
 
 # prediction of cute_dog_photo image
-pred = {"cat": str(0.00), "dog": str(99.99887)}
+pred = {"cat": str(0.00), "dog": 99.99887}
 
 # test function
 def test_predict_saved_model():
-    assert predict_saved_model(img_path='./cute_dog_photo.jpg',
-                               model_path='./model.h5')["dog"] == pred["dog"]
+    assert float(predict_saved_model(img_path='./cute_dog_photo.jpg',
+                               model_path='./model.h5')["dog"]) == approx(pred["dog"])
